2a9e92c30fe4d6d795f2b928a42719d1ffb14fc0
hoolywant <sir.saltickow@yandex.ru>
2023-05-24 13:35:45 +0300

    added first main function

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ import utils
 
 
 text = utils.open_file('operations.json')
-#print(utils.last_operations_dates(text))
-
 text1 = utils.get_last_operations(text)
-# print(text[0])
-print(text1)
 for i in text1:
-    print(i['date'])
-#
-# for i in text1:
-#     print(i)
+    print(i)
+
--- utils/utils.py
+++ utils/utils.py
@@ -6,41 +6,26 @@ def open_file(file):
         dict_ = load(text)
         return dict_
 
-# def last_operations_dates(text):
-#     dates_list = []
-#     for i in text:
-#         if i['state'] == 'EXECUTED':
-#             date = i['date'][:10]
-#             date_list = date.split('-')
-#             date_list.reverse()
-#             dates_list.append('.'.join(date_list))
-#     dates_list.sort()
-#     return dates_list
-# #
-
 
 def get_last_operations(text, count=5):
-    all_dates_list = [i['date'] for i in text if i != {}]
+    all_dates_list = []
+    for i in text:
+        if i != {} and i["state"] == "EXECUTED":
+            all_dates_list.append(i['date'])
     all_dates_list.sort()
     all_dates_list = list(reversed(all_dates_list))
     dates_list = all_dates_list[:count]
-
-    list_for_operations = []
     for i in text:
         if i != {} and i["state"] == "EXECUTED":
             if i['date'] in dates_list:
-                list_for_operations.append(i)
+                index_ = dates_list.index(i['date'])
+                dates_list[index_] = i
+    return dates_list
 
 
-    # for i in list_for_operations:
-    #     index_ = dates_list.index(i['date'])
-    #     dates_list[index_] = i
-    # return dates_list
-    return list_for_operations
-
-
-
-
-
-
-
+def get_date(date):
+    norm_date = date[:10]
+    norm_date.split('-')
+    norm_date .reverse()
+    '.'.join(norm_date )
+    return norm_date
